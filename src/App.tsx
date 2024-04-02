0a20f4eb8afa5767b843a4c9283c2a92634cb2d7
Miroslav <bangwars3@gmail.com>
2024-04-03 01:40:27 +0400

    комент не нужен

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css"
 import HeaderComponent from "./components/header/header"
 import MainPage from "./pages/main/mainPage"
 
-// выаыфва
+
 const App = () => {
   return (
       <MainPage/>
