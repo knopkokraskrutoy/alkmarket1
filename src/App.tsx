0270904ff3ca9d72e2c1420cd14d6a9dc54eadd5
arseny <arsenijpsonko4842@gmail.com>
2024-04-03 02:13:41 +0500

    123

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import HeaderComponent from "./components/header/header"
+import MainPage from "./pages/main/mainPage"
+
+
+const App = () => {
+  return (
+      <MainPage/>
+  )
+}
+
+export default App
