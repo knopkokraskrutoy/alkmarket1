3f751dc931f14d3928f39b6938446fa877fe971b
Miroslav <bangwars3@gmail.com>
2024-04-13 02:14:00 +0400

    added register to market

--- src/components/header/elements/login.tsx
+++ src/components/header/elements/login.tsx
@@ -3,6 +3,8 @@ import { Button, Checkbox, Form, type FormProps, Input } from "antd"
 import DropdownUser from "./dropDownUser"
 import { useState } from "react"
 import Link from "antd/es/typography/Link"
+import { Signup } from "../type"
+import { Register } from "./register/register"
 
 type FieldType = {
   username?: string
@@ -18,13 +20,15 @@ const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = errorInfo => {
   console.log("Failed:", errorInfo)
 }
 
+
+
 const Login: React.FC<{
-  setAuthorized: any;
-  isModalOpen: boolean;
-  setIsModalOpen: any;
-  authorized: boolean;
+  setAuthorized: any
+  isModalOpen: boolean
+  setIsModalOpen: any
+  authorized: boolean
 }> = ({ setAuthorized, authorized, isModalOpen, setIsModalOpen }) => {
-  
+  const [isLogin, setIsLogin] = useState(true)
 
   const showModal = () => {
     setIsModalOpen(true)
@@ -45,45 +49,54 @@ const Login: React.FC<{
       open={isModalOpen}
       footer={null}
       onCancel={handleButtonClick}
-      style={{ textAlign: "center", display: "flex",justifyContent:"center" }}
+      style={{ textAlign: "center", display: "flex", justifyContent: "center" }}
     >
-      <h2>Вход</h2>
-      <Form
-        name="basic"
-        layout="vertical"
-        style={{ maxWidth: 400 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label="Логин"
-          name="username"
-          rules={[{ required: true, message: "Вы не ввели логин!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Пароль"
-          name="password"
-          rules={[{ required: true, message: "Вы не ввели пароль!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-        >
-          <Checkbox>Запомнить меня</Checkbox>
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" block >Войти</Button>
-      </Form.Item>
-      <Form.Item>
-        <Link><Button type="link" block >Нет аккаунта? Зарегистрироваться</Button></Link>
-      </Form.Item>
-      </Form>
+      {isLogin ? (
+        <>
+          <h2>Вход</h2>
+          <Form
+            name="basic"
+            layout="vertical"
+            style={{ width: 400 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              label="Email"
+              name="username"
+              rules={[{ type: 'email', required: true, message: "Вы не ввели email!" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item<FieldType>
+              label="Пароль"
+              name="password"
+              rules={[{ required: true, message: "Вы не ввели пароль!" }]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item<FieldType> name="remember" valuePropName="checked">
+              <Checkbox>Запомнить меня</Checkbox>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" block>
+                Войти
+              </Button>
+            </Form.Item>
+            <Form.Item>
+                <Button type="link" block onClick={() =>setIsLogin(!isLogin)}>
+                  Нет аккаунта? Зарегистрироваться
+                </Button>
+            </Form.Item>
+          </Form>
+        </>
+      ) : (
+        <>
+          <Register isLogin={isLogin} setIsLogin={setIsLogin}/>
+        </>
+      )}
     </Modal>
   )
 }
